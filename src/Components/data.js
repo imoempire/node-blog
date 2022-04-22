85ab3ae8f15b7708c085d45d2f41c89c75e9ce1e
imoempire <mrimoempire@gmail.com>
2022-04-22 14:02:21 +0000

    create an image slider

---
+++ src/Components/data.js
@@ -0,0 +1,35 @@
+import photo from "../../assets/ice.jpg";
+import image from "../../assets/bread.jpg"
+
+export const data = [
+  {
+    id: '123ec',
+    thumbnail: photo,
+    title: "Makin Ice Cream",
+    author: "Admin",
+  },
+  {
+    id: 'bjudd',
+    thumbnail: image,
+    title: "Makin Ice Cream Rolls",
+    author: "Admin",
+  },
+  {
+    id: 'kbjsd',
+    thumbnail: photo,
+    title: "Makin the food delicious",
+    author: "Admin",
+  },
+  {
+    id: 'jbddjsdk',
+    thumbnail: image,
+    title: "Bread Making",
+    author: "Admin",
+  },
+  {
+    id: 'ljbjdslj',
+    thumbnail: image,
+    title: "Makin The perfect Rolls",
+    author: "Admin",
+  },
+];
